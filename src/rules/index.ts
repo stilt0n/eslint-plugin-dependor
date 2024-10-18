7231edc8a7160830ca47d14d1a539a26debf857a
stilt0n <mattias.leino@arts.cornish.edu>
2024-10-18 18:21:48 -0400

    export jsx rule in index file

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,7 +1,9 @@
-import { rule as noCommaSeparatedExportDeclarations } from "./no-comma-separated-export-declarations";
+import { rule as noCommaSeparatedExportDeclarations } from './no-comma-separated-export-declarations';
+import { rule as noUnquotedImportOrExportInJsx } from './no-unquoted-import-or-export-in-jsx';
 
 const rules = {
   noCommaSeparatedExportDeclarations,
+  noUnquotedImportOrExportInJsx,
 };
 
 export default rules;
